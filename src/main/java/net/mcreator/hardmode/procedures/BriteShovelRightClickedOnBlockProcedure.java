f715b19c91d4de8bd2f08e526300c85d97ac8df7
FireArrow108 <firearrow187@gmail.com>
2020-04-10 16:29:56 +0200

    Alpha_17FF Another bugfix: 	Brite shovel now tries to destroy blocks that are destroyable with shovels, and doesn't try to destroy air blocks. Hopefully...

--- src/main/java/net/mcreator/hardmode/procedures/BriteShovelRightClickedOnBlockProcedure.java
+++ src/main/java/net/mcreator/hardmode/procedures/BriteShovelRightClickedOnBlockProcedure.java
@@ -2,6 +2,7 @@ package net.mcreator.hardmode.procedures;
 
 import net.minecraft.world.World;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.block.material.Material;
 import net.minecraft.block.Block;
 
 import net.mcreator.hardmode.HardmodeElements;
@@ -36,9 +37,19 @@ public class BriteShovelRightClickedOnBlockProcedure extends HardmodeElements.Mo
 		double xC = 0;
 		double zC = 0;
 		double yC = 0;
-		xC = (double) (x + (Math.round((2 * Math.random())) - 1));
-		yC = (double) (y + (Math.round((2 * Math.random())) - 1));
-		zC = (double) (z + (Math.round((2 * Math.random())) - 1));
+		while (((!((world.getBlockState(new BlockPos((int) (xC), (int) (yC), (int) (zC)))).getMaterial() == Material.AIR)) && (((world
+				.getBlockState(new BlockPos((int) (xC), (int) (yC), (int) (zC)))).getMaterial() == Material.EARTH)
+				&& (((world.getBlockState(new BlockPos((int) (xC), (int) (yC), (int) (zC)))).getMaterial() == Material.ORGANIC) && (((world
+						.getBlockState(new BlockPos((int) (xC), (int) (yC), (int) (zC)))).getMaterial() == Material.PLANTS)
+						&& (((world.getBlockState(new BlockPos((int) (xC), (int) (yC), (int) (zC)))).getMaterial() == Material.TALL_PLANTS)
+								&& (((world.getBlockState(new BlockPos((int) (xC), (int) (yC), (int) (zC)))).getMaterial() == Material.SAND)
+										&& (((world.getBlockState(new BlockPos((int) (xC), (int) (yC), (int) (zC)))).getMaterial() == Material.SNOW)
+												&& ((world.getBlockState(new BlockPos((int) (xC), (int) (yC), (int) (zC))))
+														.getMaterial() == Material.CLAY))))))))) {
+			xC = (double) (x + (Math.round((2 * Math.random())) - 1));
+			yC = (double) (y + (Math.round((2 * Math.random())) - 1));
+			zC = (double) (z + (Math.round((2 * Math.random())) - 1));
+		}
 		Block.spawnDrops(world.getBlockState(new BlockPos((int) (xC), (int) (yC), (int) (zC))), world,
 				new BlockPos((int) (xC), (int) (yC), (int) (zC)));
 		world.destroyBlock(new BlockPos((int) (xC), (int) (yC), (int) (zC)), false);
