1f2553622ac88e2dcdf903c80a8c8c63822b73ed
FireArrow108 <firearrow187@gmail.com>
2020-04-11 14:00:48 +0200

    Alpha_19FF Another bugfix: 	Anite Sword now does the same thing (lightning)

--- src/main/java/net/mcreator/hardmode/item/AniteSwordItem.java
+++ src/main/java/net/mcreator/hardmode/item/AniteSwordItem.java
@@ -3,12 +3,16 @@ package net.mcreator.hardmode.item;
 
 import net.minecraftforge.registries.ObjectHolder;
 
+import net.minecraft.world.World;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.item.SwordItem;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Item;
 import net.minecraft.item.IItemTier;
+import net.minecraft.entity.LivingEntity;
 
+import net.mcreator.hardmode.procedures.AnitePickaxeBlockDestroyedWithToolProcedure;
 import net.mcreator.hardmode.HardmodeElements;
 
 @HardmodeElements.ModElement.Tag
@@ -46,6 +50,23 @@ public class AniteSwordItem extends HardmodeElements.ModElement {
 				return Ingredient.EMPTY;
 			}
 		}, 3, -3f, new Item.Properties().group(ItemGroup.TOOLS)) {
+			@Override
+			public boolean hitEntity(ItemStack itemstack, LivingEntity entity, LivingEntity entity2) {
+				boolean retval = super.hitEntity(itemstack, entity, entity2);
+				int x = (int) entity.posX;
+				int y = (int) entity.posY;
+				int z = (int) entity.posZ;
+				World world = entity.world;
+				{
+					java.util.HashMap<String, Object> $_dependencies = new java.util.HashMap<>();
+					$_dependencies.put("x", x);
+					$_dependencies.put("y", y);
+					$_dependencies.put("z", z);
+					$_dependencies.put("world", world);
+					AnitePickaxeBlockDestroyedWithToolProcedure.executeProcedure($_dependencies);
+				}
+				return retval;
+			}
 		}.setRegistryName("anitesword"));
 	}
 }
