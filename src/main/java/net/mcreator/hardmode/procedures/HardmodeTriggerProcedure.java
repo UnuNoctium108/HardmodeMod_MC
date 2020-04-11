8454b50cb7cc336ca111be4c611b1606add47182
FireArrow108 <firearrow187@gmail.com>
2020-04-11 15:02:30 +0200

    Alpha_20 Changelog: 	Changed registry name of A-Onite Ore 		Before: 'AOniteBlock' 		After:    'AOniteOre' 	Added advancement: 		Hardmode 			-Unlocks a new tab 			-Gives 24 XP to the player 	Deleted unused procedures and other elements

--- src/main/java/net/mcreator/hardmode/procedures/HardmodeTriggerProcedure.java
+++ src/main/java/net/mcreator/hardmode/procedures/HardmodeTriggerProcedure.java
@@ -5,13 +5,24 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.common.MinecraftForge;
 
+import net.minecraft.world.server.ServerWorld;
 import net.minecraft.world.World;
 import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.Vec2f;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.entity.boss.WitherEntity;
 import net.minecraft.entity.Entity;
+import net.minecraft.command.ICommandSource;
+import net.minecraft.command.CommandSource;
+import net.minecraft.advancements.AdvancementProgress;
+import net.minecraft.advancements.Advancement;
 
 import net.mcreator.hardmode.HardmodeElements;
+
+import java.util.Iterator;
 
 @HardmodeElements.ModElement.Tag
 public class HardmodeTriggerProcedure extends HardmodeElements.ModElement {
@@ -25,23 +36,49 @@ public class HardmodeTriggerProcedure extends HardmodeElements.ModElement {
 			System.err.println("Failed to load dependency entity for procedure HardmodeTrigger!");
 			return;
 		}
+		if (dependencies.get("x") == null) {
+			System.err.println("Failed to load dependency x for procedure HardmodeTrigger!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			System.err.println("Failed to load dependency y for procedure HardmodeTrigger!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			System.err.println("Failed to load dependency z for procedure HardmodeTrigger!");
+			return;
+		}
 		if (dependencies.get("world") == null) {
 			System.err.println("Failed to load dependency world for procedure HardmodeTrigger!");
 			return;
 		}
 		Entity entity = (Entity) dependencies.get("entity");
+		int x = (int) dependencies.get("x");
+		int y = (int) dependencies.get("y");
+		int z = (int) dependencies.get("z");
 		World world = (World) dependencies.get("world");
 		if ((entity instanceof WitherEntity)) {
-			{
-				java.util.HashMap<String, Object> $_dependencies = new java.util.HashMap<>();
-				$_dependencies.put("entity", entity);
-				$_dependencies.put("world", world);
-				HardmodeCommandExecutedProcedure.executeProcedure($_dependencies);
+			if (!world.isRemote && world.getServer() != null) {
+				world.getServer().getCommandManager().handleCommand(new CommandSource(ICommandSource.field_213139_a_, new Vec3d(x, y, z), Vec2f.ZERO,
+						(ServerWorld) world, 4, "", new StringTextComponent(""), world.getServer(), null).withFeedbackDisabled(),
+						"hardmode status set true");
 			}
 			{
 				MinecraftServer mcserv = ServerLifecycleHooks.getCurrentServer();
 				if (mcserv != null)
 					mcserv.getPlayerList().sendMessage(new StringTextComponent("The evil and darkness are rising..."));
+			}
+			if (entity instanceof ServerPlayerEntity) {
+				Advancement _adv = ((MinecraftServer) ((ServerPlayerEntity) entity).server).getAdvancementManager()
+						.getAdvancement(new ResourceLocation("hardmode:hardmodetriggeredachievement"));
+				AdvancementProgress _ap = ((ServerPlayerEntity) entity).getAdvancements().getProgress(_adv);
+				if (!_ap.isDone()) {
+					Iterator _iterator = _ap.getRemaningCriteria().iterator();
+					while (_iterator.hasNext()) {
+						String _criterion = (String) _iterator.next();
+						((ServerPlayerEntity) entity).getAdvancements().grantCriterion(_adv, _criterion);
+					}
+				}
 			}
 		}
 	}
